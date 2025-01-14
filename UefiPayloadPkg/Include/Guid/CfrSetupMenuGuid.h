a42154fbcb4818fe793d62405dcd358dc16350b3
Filip Brozovic <fbrozovic@gmail.com>
2025-01-14 23:01:03 +0100

    UefiPayloadPkg/CfrSetupMenuDxe: Implement option dependencies
    Signed-off-by: Filip Brozovic <fbrozovic@gmail.com>

--- UefiPayloadPkg/Include/Guid/CfrSetupMenuGuid.h
+++ UefiPayloadPkg/Include/Guid/CfrSetupMenuGuid.h
@@ -43,11 +43,13 @@ enum cfr_option_flags {
 #define CB_TAG_CFR_VARCHAR_UI_NAME      0x0008
 #define CB_TAG_CFR_VARCHAR_UI_HELPTEXT  0x0009
 #define CB_TAG_CFR_VARCHAR_DEF_VALUE    0x000a
+#define CB_TAG_CFR_DEP_VALUES           0x000c
 #pragma pack (1)
 typedef struct {
   UINT32  tag;          /*
                          * CFR_TAG_VARCHAR_OPT_NAME, CFR_TAG_VARCHAR_UI_NAME,
-                         * CFR_TAG_VARCHAR_UI_HELPTEXT or CFR_TAG_VARCHAR_DEF_VALUE
+                         * CFR_TAG_VARCHAR_UI_HELPTEXT, CFR_TAG_VARCHAR_DEF_VALUE
+                         * or CFR_TAG_DEP_VALUES
                          */
   UINT32  size;         /* Length of the entire structure */
   UINT32  data_length;  /* Length of data, including NULL terminator for strings */
@@ -81,6 +83,7 @@ typedef struct {
    * CFR_VARCHAR_OPT_NAME     opt_name
    * CFR_VARCHAR_UI_NAME      ui_name
    * CFR_VARCHAR_UI_HELPTEXT  ui_helptext (Optional)
+   * CFR_DEP_VALUES           dependency_values (Optional)
    * CFR_ENUM_VALUE           enum_values[]
    */
 } CFR_OPTION_NUMERIC;
@@ -96,6 +99,7 @@ typedef struct {
    * CFR_OPT_NAME     opt_name
    * CFR_UI_NAME      ui_name
    * CFR_UI_HELPTEXT  ui_helptext (Optional)
+   * CFR_DEP_VALUES   dependency_values (Optional)
    * CFR_VARCHAR      default_value
    */
 } CFR_OPTION_VARCHAR;
@@ -115,6 +119,7 @@ typedef struct {
   /*
    * CFR_UI_NAME      ui_name
    * CFR_UI_HELPTEXT  ui_helptext (Optional)
+   * CFR_DEP_VALUES   dependency_values (Optional)
    */
 } CFR_OPTION_COMMENT;
 
@@ -128,6 +133,7 @@ typedef struct {
   UINT32                flags;            /* enum cfr_option_flags */
   /*
    * CFR_UI_NAME        ui_name
+   * CFR_DEP_VALUES     dependency_values (Optional)
    * <T in CFR_OPTION>  options[]
    */
 } CFR_OPTION_FORM;
