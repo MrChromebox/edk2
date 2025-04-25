825b2a10b25ed5c2c7d94a0154062995829afbe8
Matt DeVillier <matt.devillier@gmail.com>
2025-04-25 13:15:47 -0500

    MdeModulePkg: Don't remove rejected devices
    This is needed to fix framebuffer display when using
    `CONFIG_RESOURCE_ALLOCATION_TOP_DOWN=y` in coreboot. Without this
    patch, the display backlight will be on, but no boot logo or
    menus visible.
    
    The code detecting valid BARs is buggy AF, so just skip the whole
    mess for now.
    
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- MdeModulePkg/Bus/Pci/PciBusDxe/PciEnumeratorSupport.c
+++ MdeModulePkg/Bus/Pci/PciBusDxe/PciEnumeratorSupport.c
@@ -2732,6 +2732,9 @@ IsPciDeviceRejected (
     return FALSE;
   }
 
+  // This code is buggy AF, so don't reject anything
+  return FALSE;
+
   if (IS_CARDBUS_BRIDGE (&PciIoDevice->Pci)) {
     //
     // Only test base registers for P2C
