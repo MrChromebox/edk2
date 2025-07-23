65a3c48c669b8bd8f7504083e35fb02534a9cb96
Matt DeVillier <matt.devillier@gmail.com>
2025-07-23 12:19:26 -0500

    Revert "MdeModulePkg: Don't remove rejected devices"
    This reverts commit 825b2a10b25ed5c2c7d94a0154062995829afbe8.

--- MdeModulePkg/Bus/Pci/PciBusDxe/PciEnumeratorSupport.c
+++ MdeModulePkg/Bus/Pci/PciBusDxe/PciEnumeratorSupport.c
@@ -2732,9 +2732,6 @@ IsPciDeviceRejected (
     return FALSE;
   }
 
-  // This code is buggy AF, so don't reject anything
-  return FALSE;
-
   if (IS_CARDBUS_BRIDGE (&PciIoDevice->Pci)) {
     //
     // Only test base registers for P2C
