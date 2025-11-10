bcdcbda2e0b36777dae74fcdf84fdf69a30f24de
Matt DeVillier <matt.devillier@gmail.com>
2025-11-10 15:35:46 -0600

    MdeModulePkg/BmBoot: Skip non-zero UFS LUNs when enumerating boot options
    Prevents non-bootable entries from appearing in the boot menu.
    
    Signed-off-by: Matt DeVillier <matt.devillier@gmail.com>

--- MdeModulePkg/Library/UefiBootManagerLib/BmBoot.c
+++ MdeModulePkg/Library/UefiBootManagerLib/BmBoot.c
@@ -2289,6 +2289,7 @@ BmEnumerateBootOptions (
   UINTN                         Removable;
   UINTN                         Index;
   UINTN                         EmmcCount;
+  EFI_DEVICE_PATH_PROTOCOL      *DevicePath;
   CHAR16                        *Description;
 
   ASSERT (BootOptionCount != NULL);
@@ -2319,6 +2320,11 @@ BmEnumerateBootOptions (
         continue;
       }
 
+      DevicePath = DevicePathFromHandle (Handles[Index]);
+      if (DevicePath == NULL) {
+        continue;
+      }
+
       //
       // Skip the logical partitions
       //
@@ -2345,6 +2351,29 @@ BmEnumerateBootOptions (
       if ((BlkIo->Media->RemovableMedia == TRUE) &&
           (BlkIo->Media->MediaPresent == FALSE))
       {
+        continue;
+      }
+
+      //
+      // Skip non-bootable UFS logical units (only allow LUN 0)
+      //
+      EFI_DEVICE_PATH_PROTOCOL  *Node;
+      BOOLEAN                   SkipHandle;
+
+      SkipHandle = FALSE;
+      for (Node = DevicePath; !IsDevicePathEnd (Node); Node = NextDevicePathNode (Node)) {
+        if ((DevicePathType (Node) == MESSAGING_DEVICE_PATH) &&
+            (DevicePathSubType (Node) == MSG_UFS_DP))
+        {
+          if (((UFS_DEVICE_PATH *)Node)->Lun != 0) {
+            SkipHandle = TRUE;
+          }
+
+          break;
+        }
+      }
+
+      if (SkipHandle) {
         continue;
       }
 
